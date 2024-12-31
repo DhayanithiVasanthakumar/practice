58942353c20b6017b41c00ad45b6799ad1c1c29b
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2024-12-31 10:43:16 +0530

    check VowelConsonant

---
+++ practice/src/com/low/vowelAndConsonant/Main.java
@@ -0,0 +1,10 @@
+package com.low.vowelAndConsonant;
+
+public class Main {
+
+	public static void main(String[] args) {
+		VowelConsonant vc=new VowelConsonant();
+		vc.countVowelAndConsonant("dhaya");
+	}
+
+}
