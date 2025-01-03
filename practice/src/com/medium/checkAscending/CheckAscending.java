afea04dbf32d3a19ee22a3eb645002007a499451
DhayanithiVasanthakumar <dhayanithivasanthakumar@gmail.com>
2025-01-03 21:44:39 +0530

    Change value in array

---
+++ practice/src/com/medium/checkAscending/CheckAscending.java
@@ -0,0 +1,5 @@
+package com.medium.checkAscending;
+
+public class CheckAscending {
+
+}
--- practice/src/com/medium/reverseArray/Main.java
+++ practice/src/com/medium/reverseArray/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		ReverseArray ra=new ReverseArray();
-		int arr[]= {1,2,3,4,5,6};
+		int arr[]= {1,2,3,4,5,6,7};
 		ra.reverseAnArray(arr);
 	}
 }
